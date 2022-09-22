97887097f17968cb544a5bd586390b20c00c9048
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2022-09-22 09:23:55 -0400

    Test Description rendering

--- TikTokPreviewAccessory.jsx
+++ TikTokPreviewAccessory.jsx
@@ -3,7 +3,11 @@ import { findByDisplayName, findByProps } from "@cumcord/modules/webpack";
 const MESSAGE_LINK_REGEX = /https?:\/\/(?:\w+\.)?tiktok?\.com\/.*/g;
 
 const Embed = findByDisplayName("Embed");
-const { renderVideoComponent, renderImageComponent, renderMaskedLinkComponent} = findByProps("renderImageComponent");
+const {
+  renderVideoComponent,
+  renderImageComponent,
+  renderMaskedLinkComponent,
+} = findByProps("renderImageComponent");
 
 export default function TikTokPreviewAccessory(props) {
   const { message } = props;
@@ -14,33 +18,42 @@ export default function TikTokPreviewAccessory(props) {
     elements.push(
       <Embed
         embed={{
-          type: "video",
+          //type: "video",
+          rawDescription: "",
           color: "#7401d3",
           author: {
             name: "TikTok Embed",
           },
-          feilds:[],
+          feilds: [],
           thumbnail: {
             url: "https://sf16-sg.tiktokcdn.com/obj/eden-sg/uvkuhyieh7lpqpbj/pwa/512x512.png",
           },
           video: {
             height: 300,
             width: 400,
-            proxyURL: "https://tt-embed.com/video/" + btoa(messageLinks).split('/')[0],
-            url: "https://tt-embed.com/video/" + btoa(messageLinks).split('/')[0],
+            proxyURL: "https://tt-embed.com/video/" + btoa(messageLinks).split("/")[0],
+            url: "https://tt-embed.com/video/" + btoa(messageLinks).split("/")[0],
           },
-          url:"https://tt-embed.com/video/" + btoa(messageLinks).split('/')[0],
+          url: "https://tt-embed.com/video/" + btoa(messageLinks).split("/")[0],
           footer: {
-            text: "Video URL is \n" + "https://tt-embed.com/video/"+ String(btoa(messageLinks).split('/')[0])
+            text:
+              "Video URL is \n" +
+              "https://tt-embed.com/video/" +
+              String(btoa(messageLinks).split("/")[0]),
           },
         }}
         renderImageComponent={renderImageComponent}
         renderLinkComponent={renderMaskedLinkComponent}
         renderVideoComponent={renderVideoComponent}
         //renderThumbnail={renderThumbnail}
+        renderDescription={() => (
+          <div>
+            <span>Test</span>
+          </div>
+        )}
       />
     );
-    console.log(elements[0].props)
+    console.log(elements[0].props);
     return elements;
   }
 
